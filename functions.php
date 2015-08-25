626e423eaef259f22b0853589f74774ccce2d615
Dave Clements <dave@theukedge.com>
2015-08-25 07:49:25 -0400

    Enqueue child theme with version of filemtime
    Uses filemtime to enqueue child theme CSS with version number
    equivalent to filemtime, meaning that a new version is automatically
    enqueued when the file changes

--- functions.php
+++ functions.php
@@ -10,7 +10,12 @@ add_action( 'wp_enqueue_scripts', 'load_parent_theme_css', 11 );
 
 
 function load_child_theme_css() {
-    wp_enqueue_style( 'child-theme', get_stylesheet_directory_uri() . 'css/style.css' );
+    wp_enqueue_style(
+        'child-theme',
+        get_stylesheet_directory_uri() . 'css/style.css',
+        array(),
+        filemtime( get_stylesheet_directory() . '/css/style.css' )
+    );
 }
 
 add_action( 'wp_enqueue_scripts', 'load_child_theme_css', 12 );
