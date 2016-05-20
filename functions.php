42da40331db324cc501941ecf66ef33250186421
Dave Clements <dave@theukedge.com>
2016-05-20 13:01:49 -0400

    Add documentation

--- functions.php
+++ functions.php
@@ -1,7 +1,29 @@
 <?php
+/**
+ * Child Theme Template
+ *
+ * This child theme template is the very least you need to start building a child theme.
+ * Simply upload this theme to your site and edit the 'template' in style.css to the
+ * folder name of the parent theme. Then activate this theme. More details on how to
+ * use this theme can be found @link https://dave.pe/2014wctpa.
+ *
+ * @package    Child Theme Template
+ * @subpackage Functions
+ * @author     Dave Clements <dave@theukedge.com>
+ * @copyright  Copyright (c) 2016 Dave Clements
+ * @version    1.2
+ * @since      1.0
+ * @link       https://github.com/theukedge/child-theme-template
+ * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ */
 
-// For more information on how to use this template, go to https://www.doitwithwp.com/putting-things-where-they-belong/
-
+/**
+ * Load parent theme css
+ *
+ * @since      2016-05-20
+ * @author     Dave Clements <dave@theukedge.com>
+ * @return     void
+ */
 function load_parent_theme_css() {
 	$parentcss = get_template_directory() . '/style.css';
 	wp_enqueue_style(
@@ -14,7 +36,13 @@ function load_parent_theme_css() {
 
 add_action( 'wp_enqueue_scripts', 'load_parent_theme_css', 11 );
 
-
+/**
+ * Load child theme css
+ *
+ * @since      2016-05-20
+ * @author     Dave Clements <dave@theukedge.com>
+ * @return     void
+ */
 function load_child_theme_css() {
 	$childcss = get_stylesheet_directory() . '/css/style.css';
 	wp_enqueue_style(
